984f1c00db1b5029dac1625fd9039eb356f628a9
Rahul kumar maury <rahu7827759@gmail.com>
2024-06-27 19:17:55 +0530

    Quiz App UI/UX design

---
+++ quize-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Quiz from "./components/Quiz";
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App;
